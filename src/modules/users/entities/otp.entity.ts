9876b989e163d30f2120ff0beb690481c0bb5ca5
Hasan Lashgari <hasanlashgari01@gmail.com>
2025-08-12 10:17:53 +0330

    change epxires_date to expires_in

--- src/modules/users/entities/otp.entity.ts
+++ src/modules/users/entities/otp.entity.ts
@@ -3,20 +3,20 @@ import { User } from "./user.entity";
 
 @Entity()
 export class Otp {
-  @PrimaryGeneratedColumn("increment")
-  id: number;
+    @PrimaryGeneratedColumn("increment")
+    id: number;
 
-  @Column({ length: 4 })
-  code: string;
+    @Column({ length: 4 })
+    code: string;
 
-  @Column()
-  expiry_date: Date;
+    @Column()
+    expires_In: Date;
 
-  @Column()
-  is_used: boolean;
+    @Column()
+    is_used: boolean;
 
-  @Column()
-  userId: number;
-  @OneToOne(() => User, (user) => user.otp, { onDelete: "CASCADE" })
-  user: User;
+    @Column()
+    userId: number;
+    @OneToOne(() => User, (user) => user.otp, { onDelete: "CASCADE" })
+    user: User;
 }
