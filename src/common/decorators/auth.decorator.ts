5777bf0315aa609a6523ae27d42761f373cc8b22
Hasan Lashgari <hasanlashgari01@gmail.com>
2025-08-18 21:16:50 +0330

    decorator for roles access

--- src/common/decorators/auth.decorator.ts
+++ src/common/decorators/auth.decorator.ts
@@ -1,7 +1,14 @@
 import { applyDecorators, UseGuards } from "@nestjs/common";
 import { ApiBearerAuth } from "@nestjs/swagger";
-import { AuthGuard } from "src/modules/auth/guards/auth.guard";
+import { JwtAuthGuard } from "src/modules/auth/guards/jwt-auth.guard";
+import { Role } from "../enum/user.enum";
+import { RolesGuard } from "src/modules/auth/guards/roles.guard";
+import { Roles } from "src/modules/auth/decorators/roles.decorator";
 
 export function Authorization() {
-  return applyDecorators(ApiBearerAuth("Authorization"), UseGuards(AuthGuard));
+  return applyDecorators(ApiBearerAuth("Authorization"), UseGuards(JwtAuthGuard));
+}
+
+export function RoleAccess(...roles: Role[]) {
+  return applyDecorators(ApiBearerAuth("Authorization"), UseGuards(RolesGuard), Roles(...roles));
 }
