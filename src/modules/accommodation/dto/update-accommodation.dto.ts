ef65395c39d94b4e61fb100f67a8230b4c26bad3
Hasan Lashgari <hasanlashgari01@gmail.com>
2025-08-20 14:58:45 +0330

    delete accommodation module

--- src/modules/accommodation/dto/update-accommodation.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateAccommodationDto } from './create-accommodation.dto';
-
-export class UpdateAccommodationDto extends PartialType(CreateAccommodationDto) {}
