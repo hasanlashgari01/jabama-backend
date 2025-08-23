b7a4ec4500b2582dcffcd55830775df3b206b3ef
Hasan Lashgari <hasanlashgari01@gmail.com>
2025-08-23 17:02:28 +0330

    import amenity category and amenity

--- src/modules/stay/stay.module.ts
+++ src/modules/stay/stay.module.ts
@@ -1,12 +1,20 @@
 import { Module } from "@nestjs/common";
-import { StayService } from "./stay.service";
-import { StayController } from "./stay.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { S3Service } from "../s3/s3.service";
+import { AmenityCategoryController } from "./controllers/amenity-category.controller";
+import { AmenityController } from "./controllers/amenity.controller";
+import { StayController } from "./controllers/stay.controller";
+import { StayPhoto } from "./entities/stay-photo.entity";
 import { Stay } from "./entities/stay.entity";
+import { AmenityCategoryService } from "./services/amenity-category.service";
+import { AmenityService } from "./services/amenity.service";
+import { StayService } from "./services/stay.service";
+import { AmenityCategory } from "./entities/amenity-category.entity";
+import { Amenity } from "./entities/amenity.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Stay])],
-  controllers: [StayController],
-  providers: [StayService],
+  imports: [TypeOrmModule.forFeature([Stay, StayPhoto, AmenityCategory, Amenity])],
+  controllers: [StayController, AmenityCategoryController, AmenityController],
+  providers: [StayService, AmenityCategoryService, AmenityService, S3Service],
 })
 export class StayModule {}
