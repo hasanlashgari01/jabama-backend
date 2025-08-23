ec6e08726917c29aef9d1104032478004f83740c
Hasan Lashgari <hasanlashgari01@gmail.com>
2025-08-23 16:58:54 +0330

    feat: find by id and remove

--- src/modules/stay/services/amenity-category.service.ts
+++ src/modules/stay/services/amenity-category.service.ts
@@ -1,4 +1,9 @@
-import { ConflictException, Injectable } from "@nestjs/common";
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { AmenityCategory } from "../entities/amenity-category.entity";
@@ -25,6 +30,25 @@ export class AmenityCategoryService {
 
     return {
       message: "دسته بندی با موفقیت ایجاد شد",
+    };
+  }
+
+  async findByIdOrFail(id: number) {
+    const category = await this.amenityCategoryRepository.findOneBy({ id });
+    if (!category) throw new NotFoundException("دسته بندی مورد نظر یافت نشد");
+
+    return category;
+  }
+
+  async remove(id: number) {
+    await this.findByIdOrFail(id);
+
+    const category = await this.amenityCategoryRepository.delete(id);
+    if (category.affected === 0) throw new NotFoundException("دسته بندی مورد نظر یافت نشد");
+
+    return {
+      message: "دسته بندی با موفقیت حذف شد",
+      category,
     };
   }
 }
