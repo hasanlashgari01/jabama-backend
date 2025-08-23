eab6f3368338be927eba54fd83a690980a28d22b
Hasan Lashgari <hasanlashgari01@gmail.com>
2025-08-23 17:00:26 +0330

    change dir

---
+++ src/modules/stay/controllers/stay.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from "@nestjs/common";
+import { FilesInterceptor } from "@nestjs/platform-express";
+import { ApiConsumes } from "@nestjs/swagger";
+import { RoleAccess } from "src/common/decorators/auth.decorator";
+import { FormType } from "src/common/enum/form-type.enum";
+import { Role } from "src/common/enum/user.enum";
+import { FileValidationPipe } from "src/common/validations/file.validation";
+import { CreateStayDto } from "../dto/stay.dto";
+import { UpdateStayDto } from "../dto/amenity.dto";
+import { StayService } from "../services/stay.service";
+
+@Controller("stay")
+export class StayController {
+  constructor(private readonly stayService: StayService) {}
+
+  @Post()
+  @RoleAccess(Role.HOST)
+  @ApiConsumes(FormType.Multipart)
+  @UseInterceptors(FilesInterceptor("images"))
+  create(
+    @Body() createStayDto: CreateStayDto,
+    @UploadedFiles(new FileValidationPipe({ isArray: true })) files: Array<Express.Multer.File>,
+  ) {
+    return this.stayService.create(createStayDto, files);
+  }
+
+  @Get()
+  findAll() {
+    return this.stayService.findAll();
+  }
+
+  @Get(":id")
+  findOne(@Param("id") id: string) {
+    return this.stayService.findOne(+id);
+  }
+
+  @Patch(":id")
+  @RoleAccess(Role.HOST)
+  @ApiConsumes(FormType.Multipart)
+  update(@Param("id") id: string, @Body() updateStayDto: UpdateStayDto) {
+    return this.stayService.update(+id, updateStayDto);
+  }
+}
---
+++ src/modules/stay/services/stay.service.ts
@@ -0,0 +1,91 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { CreateStayDto } from "../dto/stay.dto";
+import { UpdateStayDto } from "../dto/amenity.dto";
+import { Stay } from "../entities/stay.entity";
+import { S3Service } from "../../s3/s3.service";
+import { StayPhoto } from "../entities/stay-photo.entity";
+import { v4 as uuidv4 } from "uuid";
+
+@Injectable()
+export class StayService {
+  constructor(
+    @InjectRepository(Stay) private stayRepository: Repository<Stay>,
+    @InjectRepository(StayPhoto) private stayPhotoRepository: Repository<StayPhoto>,
+    private s3Service: S3Service,
+  ) {}
+
+  async create(createStayDto: CreateStayDto, files: Array<Express.Multer.File>) {
+    const {
+      name,
+      address,
+      meterage,
+      area,
+      latitude,
+      longitude,
+      type,
+      description,
+      description_space,
+      description_shared_space,
+      description_additional,
+      host_id,
+      city_id,
+    } = createStayDto;
+
+    const newStay = await this.stayRepository.create({
+      name,
+      slug: this.generateUniqueSlug(),
+      address,
+      meterage,
+      area,
+      latitude,
+      longitude,
+      type,
+      description,
+      description_space,
+      description_shared_space,
+      description_additional,
+      host_id,
+      city_id,
+    });
+
+    await this.uploadFiles(newStay.id, files);
+
+    return "This action adds a new stay";
+  }
+
+  findAll() {
+    return `This action returns all stay`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} stay`;
+  }
+
+  update(id: number, updateStayDto: UpdateStayDto) {
+    return `This action updates a #${id} stay`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} stay`;
+  }
+
+  generateUniqueSlug(): string {
+    return uuidv4().split("-")[0];
+  }
+
+  async uploadFiles(stay_id: number, files: Array<Express.Multer.File>) {
+    await Promise.all(
+      files.map(async (file) => {
+        const { Location, Key } = await this.s3Service.uploadFile(file, "images");
+
+        await this.stayPhotoRepository.create({
+          url: Location,
+          key: Key,
+          stay_id,
+        });
+      }),
+    );
+  }
+}
