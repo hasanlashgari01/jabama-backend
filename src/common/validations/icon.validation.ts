3d61e2924d3d5aba3cdaf69386f343b2ae4394f7
Hasan Lashgari <hasanlashgari01@gmail.com>
2025-08-25 11:59:41 +0330

    split file to icon and image

--- src/common/validations/icon.validation.ts
+++ src/common/validations/icon.validation.ts
@@ -7,26 +7,24 @@ interface FileValidationOptions {
 }
 
 @Injectable()
-export class FileValidationPipe implements PipeTransform {
+export class IconValidationPipe implements PipeTransform {
   private readonly maxSize: number;
   private readonly allowedTypes: string[];
   private readonly isArray: boolean;
 
   constructor(options?: FileValidationOptions) {
     this.maxSize = options?.maxSize ?? 1 * 1024 * 1024;
-    this.allowedTypes = options?.allowedTypes ?? ["image/png", "image/jpg", "image/jpeg"];
+    this.allowedTypes = options?.allowedTypes ?? ["image/svg+xml", "image/png", "image/x-icon"];
     this.isArray = options?.isArray ?? false;
   }
 
   transform(file: Express.Multer.File | Express.Multer.File[], metadata: ArgumentMetadata) {
-    if (!file) {
-      throw new BadRequestException("هیچ فایلی ارسال نشده است");
-    }
+    if (!file) throw new BadRequestException("فیلد آیکون اجباری است");
 
     if (this.isArray) {
       const files = file as Express.Multer.File[];
       if (files.length === 0) {
-        throw new BadRequestException("لیست فایل‌ها خالی است");
+        throw new BadRequestException("لیست آیکون خالی است");
       }
       files.forEach((file) => this.validateFile(file));
       return files;
@@ -38,13 +36,13 @@ export class FileValidationPipe implements PipeTransform {
   private validateFile(file: Express.Multer.File): Express.Multer.File {
     if (file.size > this.maxSize) {
       throw new BadRequestException(
-        `حجم فایل نباید بیشتر از ${(this.maxSize / (1024 * 1024)).toFixed(1)} مگابایت باشد`,
+        `حجم آیکون نباید بیشتر از ${(this.maxSize / (1024 * 1024)).toFixed(1)} مگابایت باشد`,
       );
     }
 
     if (!this.allowedTypes.includes(file.mimetype)) {
       throw new BadRequestException(
-        `فرمت فایل معتبر نیست. فقط ${this.allowedTypes.join(", ")} مجاز هستند`,
+        `فرمت آیکون معتبر نیست. فقط ${this.allowedTypes.join(", ")} مجاز هستند`,
       );
     }
 
---
+++ src/common/validations/image.validation.ts
@@ -0,0 +1,51 @@
+import { Injectable, PipeTransform, ArgumentMetadata, BadRequestException } from "@nestjs/common";
+
+interface FileValidationOptions {
+  maxSize?: number; // bytes
+  allowedTypes?: string[];
+  isArray?: boolean;
+}
+
+@Injectable()
+export class ImageValidationPipe implements PipeTransform {
+  private readonly maxSize: number;
+  private readonly allowedTypes: string[];
+  private readonly isArray: boolean;
+
+  constructor(options?: FileValidationOptions) {
+    this.maxSize = options?.maxSize ?? 1 * 1024 * 1024;
+    this.allowedTypes = options?.allowedTypes ?? ["image/png", "image/jpg", "image/jpeg"];
+    this.isArray = options?.isArray ?? false;
+  }
+
+  transform(file: Express.Multer.File | Express.Multer.File[], metadata: ArgumentMetadata) {
+    if (!file) throw new BadRequestException("هیچ فایلی ارسال نشده است");
+
+    if (this.isArray) {
+      const files = file as Express.Multer.File[];
+      if (files.length === 0) {
+        throw new BadRequestException("لیست فایل‌ها خالی است");
+      }
+      files.forEach((file) => this.validateFile(file));
+      return files;
+    }
+
+    return this.validateFile(file as Express.Multer.File);
+  }
+
+  private validateFile(file: Express.Multer.File): Express.Multer.File {
+    if (file.size > this.maxSize) {
+      throw new BadRequestException(
+        `حجم فایل نباید بیشتر از ${(this.maxSize / (1024 * 1024)).toFixed(1)} مگابایت باشد`,
+      );
+    }
+
+    if (!this.allowedTypes.includes(file.mimetype)) {
+      throw new BadRequestException(
+        `فرمت فایل معتبر نیست. فقط ${this.allowedTypes.join(", ")} مجاز هستند`,
+      );
+    }
+
+    return file;
+  }
+}
